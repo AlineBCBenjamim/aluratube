ffcf5bae5e3b21dbc03b6e6032e4f5f6cf4a1b1b
Aline Carvalho <alineoak9@gmail.com>
2022-11-08 20:58:39 -0300

    Favoritos e banner concluido do desafio

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
+import { StyledFavoriteList } from "../src/components/FavoriteList";
 
 function HomePage() {
   const estiloDaHomePage = {
@@ -25,6 +26,7 @@ function HomePage() {
         <Menu />
         <Header />
         <Timeline playlists={config.playlists}>conteúdo</Timeline>
+        <FavoriteList favorites={config.favorites} />
       </div>
     </>
   );
@@ -41,7 +43,13 @@ export default HomePage;
 // }
 
 const StyledHeader = styled.div`
-  img {
+.banner{
+  width: 100%;
+  height: 24rem;
+  object-fit: cover;
+  object-position: top;
+}
+  .foto-perfil {
     width: 80px;
     height: 80px;
     border-radius: 50%;
@@ -59,12 +67,17 @@ const StyledHeader = styled.div`
 function Header() {
   return (
     <StyledHeader>
-      {/* <img src="banner" alt="banner"/> */}
+      <img
+        src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        alt="banner"
+        className="banner"
+      />
 
       <section className="user-info">
         <img
           src={`https://github.com/${config.github}.png`}
           alt="foto do perfil"
+          className="foto-perfil"
         />
         <div>
           <h2>{config.name}</h2>
@@ -102,4 +115,29 @@ function Timeline(props) {
     })}    
     </StyledTimeline>
     );
+}
+
+
+function FavoriteList(props){
+  return (
+    <StyledFavoriteList>
+      <h2>Favoritos</h2>
+      <div className="favorite-item">
+        {props.favorites.map((favorite, index) => {
+          return (
+            <section key={index}>
+              <a
+                href={`https://www.youtube.com/user/${favorite.nickname}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={favorite.image} alt="imagem" />
+                <span>{favorite.nickname}</span>
+              </a>
+            </section>
+          );
+        })}
+      </div>
+    </StyledFavoriteList>
+  );
 }
